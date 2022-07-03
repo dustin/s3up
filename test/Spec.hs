d7413969fdc71b5439915bbb83771a391fa55f39
Dustin Sallings <dustin@spy.net>
2022-07-02 18:51:22 -1000

    Use tasty discover

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,18 +1,12 @@
+module Spec where
+
 import           Test.Tasty
 import           Test.Tasty.HUnit
 import           Test.Tasty.QuickCheck as QC
 
 import           S3Up
 
-testMkObjectKey :: Assertion
-testMkObjectKey = do
+unit_MkObjectKey :: Assertion
+unit_MkObjectKey = do
   assertEqual "full path" "tmp/file.txt" (mkObjectKey "/some/file.txt" "tmp/file.txt")
   assertEqual "partial path" "tmp/file.txt" (mkObjectKey "/some/file.txt" "tmp/")
-
-tests :: [TestTree]
-tests = [
-  testCase "mkObjectKey" testMkObjectKey
-    ]
-
-main :: IO ()
-main = defaultMain $ testGroup "All Tests" tests
