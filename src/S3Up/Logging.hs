c4e6da3ec5b4e1a230bbadccd1645c7c5322751e
Dustin Sallings <dustin@spy.net>
2024-09-18 19:42:51 -1000

    Logger doesn't need IO

--- src/S3Up/Logging.hs
+++ src/S3Up/Logging.hs
@@ -13,13 +13,30 @@ module S3Up.Logging where
 
 import           Cleff
 import           Control.Monad         (when)
-import           Control.Monad.Logger  (Loc (..), LogLevel (..), LogSource, LogStr, fromLogStr)
+import           Control.Monad.Logger  (Loc (..), LogLevel (..), LogSource, LogStr, defaultLoc, fromLogStr)
 import qualified Data.ByteString.Char8 as C8
 import           Data.Foldable         (fold)
 import           Data.String           (fromString)
 import qualified Data.Text             as T
 import           S3Up.Effects
 import           System.IO             (stderr)
+
+genericLog :: LogLevel -> LogFX :> es => T.Text -> Eff es ()
+genericLog lvl = logFX defaultLoc "" lvl . fromString . T.unpack
+
+logError, logInfo, logDbg :: LogFX :> es => T.Text -> Eff es ()
+
+logErrorL, logInfoL, logDbgL :: (Foldable f, LogFX :> es) => f T.Text -> Eff es ()
+
+logErrorL = logError . fold
+logInfoL = logInfo . fold
+logDbgL = logDbg . fold
+
+logError = genericLog LevelError
+
+logInfo = genericLog LevelInfo
+
+logDbg = genericLog LevelDebug
 
 baseLogger :: LogLevel -> Loc -> LogSource -> LogLevel -> LogStr -> IO ()
 baseLogger minLvl _ _ lvl s = when (lvl >= minLvl) $ C8.hPutStrLn stderr (fromLogStr ls)
@@ -36,21 +53,6 @@ runLogFX :: (IOE :> es) => Bool -> Eff (LogFX : es) a -> Eff es a
 runLogFX verbose = interpretIO \case
   LogFX loc src lvl' msg -> liftIO $ baseLogger minLvl loc src lvl' msg
   where minLvl = if verbose then LevelDebug else LevelInfo
-
--- Text loggers
-logError, logInfo, logDbg :: [IOE, LogFX] :>> es => T.Text -> Eff es ()
-
--- List loggers
-logErrorL, logInfoL, logDbgL :: (Foldable f, [IOE, LogFX] :>> es) => f T.Text -> Eff es ()
-
-logError = liftIO . print
-logErrorL = logError . fold
-
-logInfo = liftIO . print
-logInfoL = logInfo . fold -- logInfoN . fold
-
-logDbg = liftIO . print
-logDbgL = logDbg . fold -- logDebugN . fold
 
 tshow :: Show a => a -> T.Text
 tshow = T.pack . show
